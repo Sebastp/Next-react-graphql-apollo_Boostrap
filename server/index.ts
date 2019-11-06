986ed816f150933ec2aa48bb3ff006fe7b8391d9
SEBASTIAN-PC\Sebastian <sebastianpob@gmail.com>
2019-11-06 23:04:25 +0100

    Express server on the backend

---
+++ server/index.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import next from 'next'
+
+const port = parseInt(process.env.PORT || '3000', 10)
+const dev = process.env.NODE_ENV !== 'production'
+
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.get('*', (req, res) => handle(req, res))
+  server.listen(port, err => {
+    if (err) throw err
+  })
+})
